04dc480471329d94336c08f06c11a52ae8b6700c
Lalit Gurjar <83588799+Lalit3016@users.noreply.github.com>
2021-10-23 08:17:58 +0530

    Add files via upload

---
+++ Quiz Game Using javascript/swale.js
@@ -0,0 +1,81 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which one is NOT a legal variable name?",
+    answer: "_myvar",
+    options: [
+      "_myvar",
+      "Myvar",
+      "my-var",
+      "my_var"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the correct file extension for Python files?",
+    answer: ".py",
+    options: [
+      ".pt",
+      ".py",
+      ".ph",
+      ".pvt"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which method can be used to remove any whitespace from both the beginning and the end of a string?",
+    answer: "strip()",
+    options: [
+      "trim()",
+      "strip()",
+      "ptrim()",
+      "len()"
+    ]
+  },
+  {
+      numb: 7,
+      question:"How do you create a variable with the numeric value 5?",
+      answer: "int x=5",
+      options: [
+        "int x=5",
+        "num x=5",
+        "x=5",
+        "float x=5"
+      ]
+
+    
+  }
+ ];
